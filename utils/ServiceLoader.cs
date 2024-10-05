22fe8aad2641ed456015a3f9336d553d32ca4ef0
Yehor <egorkorsungit@gmail.com>
2024-10-05 18:02:26 +0200

    Added ServiceLoader which registering services

---
+++ utils/ServiceLoader.cs
@@ -0,0 +1,11 @@
+using BooksLib.services;
+
+namespace BooksLib.utils;
+
+public static class ServiceLoader
+{
+    public static void Register(WebApplicationBuilder builder){
+        builder.Services.AddSingleton<BookService>();
+    }
+
+}
