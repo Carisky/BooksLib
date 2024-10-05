50cfdd92e31b12ba1012d7a7e670932dd4da2e95
Yehor <egorkorsungit@gmail.com>
2024-10-05 18:01:27 +0200

    Load book from directory and send to frontend

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using BooksLib.config;
+using BooksLib.services;
+using BooksLib.models;
+
 namespace BooksLib.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly BookService _bookService;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    
+    public Book CurrentBook { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, BookService bookService)
     {
         _logger = logger;
+        _bookService = bookService;
     }
 
     public void OnGet()
     {
-        
+        CurrentBook = _bookService.ReadById(1);
+        _logger.LogInformation("Loaded book with ID: {BookId}", CurrentBook.Id);
     }
 }
