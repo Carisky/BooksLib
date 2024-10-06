f8c6f6970a32f19964a0573f1ea0cb21c017b7e4
Yehor <egorkorsungit@gmail.com>
2024-10-07 00:21:25 +0200

    implemented user register logic

--- Pages/Auth/Register.cshtml.cs
+++ Pages/Auth/Register.cshtml.cs
@@ -1,23 +1,41 @@
+using BooksLib.models;
+using BooksLib.services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-namespace BooksLib.Pages.Auth{
-public class RegisterModel : PageModel
+namespace BooksLib.Pages.Auth
 {
-    [BindProperty]
-    public RegisterInputModel Input { get; set; }
-
-    public class RegisterInputModel
+    public class RegisterModel : PageModel
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
 
-    public IActionResult OnPost()
-    {
-        // Логика регистрации пользователя
-        // Добавьте проверку и сохранение нового пользователя
-        return RedirectToPage("/Auth/Login");
+        private readonly UserService _userService;
+
+        public RegisterModel(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public RegisterInputModel Input { get; set; }
+
+        public class RegisterInputModel
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (_userService.IsUserAlreadyExist(Input.UserName))
+            {
+                ModelState.AddModelError(string.Empty, "User Already Exist");
+                return Page();
+            }
+            else
+            {
+                _userService.WriteNew(Input.UserName, Input.Password);
+                return RedirectToPage("/Auth/Login");
+            }
+        }
     }
-}
 
 }
