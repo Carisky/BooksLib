216fa0392483782f04d0a9bc8d1a7dfef8d6e498
Yehor <egorkorsungit@gmail.com>
2024-10-06 15:08:22 +0200

    Added path to store users

--- config/Config.cs
+++ config/Config.cs
@@ -3,5 +3,5 @@
 public static class Config
 {
     public static string BooksStoragePath { get; } = "~\\..\\bin\\Debug\\net8.0\\storage\\books";
-
+    public static string UsersStoragePath { get;} = "~\\..\\bin\\Debug\\net8.0\\storage\\users";
 }
